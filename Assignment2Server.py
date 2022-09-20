7b106183a5c4d0961791c90341e79c2889dc39a1
DylanDockery <88761354+DylanDockery@users.noreply.github.com>
2022-09-20 03:50:31 -0400

    Update Assignment2Server.py

--- Assignment2Server.py
+++ Assignment2Server.py
@@ -9,6 +9,37 @@ from socket import *
 import argparse
 from datetime import datetime
 
+def logRequest(requestMethod,responseCode):
+    f=open("serverLog.txt","a")
+    f.write(str(datetime.now())+' '+requestMethod+' '+responseCode)
+    f.close()
+
+def response(message):
+    requestMethod=message.decode()
+    if requestMethod == 'IP':
+        logRequest(requestMethod,'OK')
+        return returnMessage = 'OK_' + clientAddress[0]
+    elif requestMethod == 'PORT':
+        logRequest(requestMethod,'OK')
+        return returnMessage = 'OK_'+ str(clientAddress[1])
+    elif requestMethod == 'TIMEDELAY':
+        #time from client and current time 
+        try:
+            time=datetime.now()
+            time_sec=time.timestamp()
+            senttime=float(message.decode().split('_')[1])
+            deltatime= time_sec - senttime
+            logRequest(requestMethod,'OK')
+            return returnMessage = 'OK_'+str(time)+'_'+str(deltatime)+'_'+str(time_sec)
+        except:
+            logRequest(requestMethod,'INVALID')
+            return returnMessage = 'INVALID'
+            
+    else:
+        logRequest(requestMethod,'INVALID')
+        return returnMessage = 'INVALID'
+
+    
 #command line argmuent declaration
 parser = argparse.ArgumentParser(description='Server')
 parser.add_argument('-t', type=str, default='TCP', help='Communication protocol that can be set to either TCP or UDP. Default is TCP')
@@ -32,22 +63,7 @@ if args.t == 'UDP':
     print("The server is ready to receive")
     while True:
         message, clientAddress = serverSocket.recvfrom(2048)
-        
-        if message.decode() == 'IP':
-            returnMessage = clientAddress[0]
-        elif message.decode() == 'PORT':
-            returnMessage = str(clientAddress[1])
-        else:
-            #time from client and current time 
-            time=datetime.now()
-            time_sec=time.timestamp()
-            senttime=float(message.decode().split('_')[1])
-            deltatime= time_sec - senttime
-            
-            returnMessage = str(time)+'_'+str(deltatime)+'_'+str(time_sec)
-
-            
-
+        returnMesage= response(message)
         serverSocket.sendto(returnMessage.encode(),clientAddress)
     
 
@@ -62,18 +78,7 @@ else:
         
         connectionSocket, addr = serverSocket.accept()
         message = connectionSocket.recv(1024).decode()
-        if message == 'IP':
-            returnMessage = addr[0]
-        elif message == 'PORT':
-            returnMessage = str(addr[1])
-        else:
-            #time from client and current time
-            time=datetime.now()
-            time_sec=time.timestamp()
-            senttime=float(message.split('_')[1])
-            deltatime= time_sec - senttime
-            
-            returnMessage = str(time)+'_'+str(deltatime)+'_'+str(time_sec)
+        returnMesage= response(message)
 
         connectionSocket.send(returnMessage.encode())
         connectionSocket.close()
